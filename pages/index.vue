6ccc18ce517b868c4247bf4416825ec9d3e6512e
Enderson Vizcaino <enjaviga@gmail.com>
2019-01-09 22:00:17 -0500

    Pagina inicio

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,13 @@
 <template>
   <section class="home">
     <div class="message">
-      <cite>La vida sería mucho más sencilla si pudiéramos echar un vistazo al código fuente.</cite>
+      <cite>&quot;La vida sería mucho más sencilla si pudiéramos echar un vistazo al código fuente&quot;</cite>
     </div>
-    <div class="yo">
-      <span>&copy; ENDERSON</span>
-      <strong>VIZCAINO</strong>
+    <div class="yo typewriter">
+      <span>
+        &copy; ENDERSON
+        <strong>VIZCAINO</strong>
+      </span>
     </div>
   </section>
 </template>
@@ -44,6 +46,38 @@ export default {
   width: 60%;
   text-align: center;
   font-family: Inconsolata, "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+}
+
+.typewriter span {
+  color: #fff;
+  font-family: monospace;
+  overflow: hidden; /* Ensures the content is not revealed until the animation */
+  border-right: 0.15em solid orange; /* The typwriter cursor */
+  white-space: nowrap; /* Keeps the content on a single line */
+  margin: 0 auto; /* Gives that scrolling effect as the typing happens */
+  letter-spacing: 0.15em; /* Adjust as needed */
+  animation: typing 3.5s steps(30, end), blink-caret 0.5s step-end infinite;
+}
+
+/* The typing effect */
+@keyframes typing {
+  from {
+    width: 0;
+  }
+  to {
+    width: 100%;
+  }
+}
+
+/* The typewriter cursor effect */
+@keyframes blink-caret {
+  from,
+  to {
+    border-color: transparent;
+  }
+  50% {
+    border-color: orange;
+  }
 }
 
 @media screen and (max-width: 48em) {
